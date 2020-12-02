36dd4e175fc78cc0f8005d7bdf1e7b04e2c311e5
Eric Adler <Eric Adler>
2020-12-02 00:35:31 -0500

    2020: 2-1: first trry

--- 2020/2/2-1.py
+++ 2020/2/2-1.py
@@ -9,6 +9,7 @@ def passwordcheck(filename):
     data_max = []
     data_char = []
     data_pass = []
+    data_split = []
     inputfile = open(filename)
     inputdata = inputfile.readlines()
     for item in inputdata:
@@ -18,9 +19,27 @@ def passwordcheck(filename):
         data_max.append(dataset_minmax[1])
         data_char.append(dataset[1][:1])
         data_pass.append(dataset[2])
-    print(data_min[0:3])
-    print(data_max[0:3])
-    print(data_char[0:3])
-    print(data_pass[0:3])
+        data_split.append([dataset_minmax[0], dataset_minmax[1], dataset[1][:1],dataset[2]])
+    print(inputdata[0:3])
+    print(data_split[0:3])
+    countgood = 0
+    countbad = 0
+    count = 0
+    for item in data_split:
+        allow_min = item[0]
+        allow_max = item[1]
+        allow_char = item[2]
+        checkpass = item[3]
+        allow_count = checkpass.count(allow_char)
+        if (allow_max < allow_count) or (allow_min > allow_count):
+            countbad += 1
+        else:
+            countgood += 1
+        count += 1
 
+    print("count good: %s", countgood)
+    print("count bad: %s", countbad)
+    print("count: %s", count)
+    print("total: %s", (countgood+countbad))
+        
 passwordcheck(inputfile_source)
