2dba7d39e2e70e26341ffec7ee441ce02e093749
Eric Adler <Eric Adler>
2020-12-02 00:29:53 -0500

    2020: 2 - don't overload assign in python.  Funky things happen.

--- 2020/2/2-1.py
+++ 2020/2/2-1.py
@@ -5,16 +5,22 @@ print "importing"
 inputfile_source = "2020/2/input.txt"
 
 def passwordcheck(filename):
-    data_min = data_max = data_char = data_pass = []
+    data_min = []
+    data_max = []
+    data_char = []
+    data_pass = []
     inputfile = open(filename)
     inputdata = inputfile.readlines()
     for item in inputdata:
         dataset = item.split(" ")
-        dataset_minmax = dataset[1].split("-")
-        data_min.append(dataset_minmax[1])
-        data_max.append(dataset_minmax[2])
-        data_char.append(dataset[2][:1])
-        data_pass.append(dataset[3])
-    print(data_char)
+        dataset_minmax = dataset[0].split("-")
+        data_min.append(dataset_minmax[0])
+        data_max.append(dataset_minmax[1])
+        data_char.append(dataset[1][:1])
+        data_pass.append(dataset[2])
+    print(data_min[0:3])
+    print(data_max[0:3])
+    print(data_char[0:3])
+    print(data_pass[0:3])
 
 passwordcheck(inputfile_source)
