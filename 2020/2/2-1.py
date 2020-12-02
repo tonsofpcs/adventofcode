4385de1ca976177473a25debfbe6aa661fd4b088
Eric Adler <Eric Adler>
2020-12-02 00:26:01 -0500

    test better.

--- 2020/2/2-1.py
+++ 2020/2/2-1.py
@@ -5,9 +5,16 @@ print "importing"
 inputfile_source = "2020/2/input.txt"
 
 def passwordcheck(filename):
+    data_min = data_max = data_char = data_pass = []
     inputfile = open(filename)
-    inputdata = inputfile.readlines().split(" ")
-    inputdata1 = inputdata[1]
-    inputdata2 = inputdata[2]
-    inputdata3 = inputdata[3]
-    print(inputdata1)
+    inputdata = inputfile.readlines()
+    for item in inputdata:
+        dataset = item.split(" ")
+        dataset_minmax = dataset[1].split("-")
+        data_min.append(dataset_minmax[1])
+        data_max.append(dataset_minmax[2])
+        data_char.append(dataset[2][:1])
+        data_pass.append(dataset[3])
+    print(data_char)
+
+passwordcheck(inputfile_source)
