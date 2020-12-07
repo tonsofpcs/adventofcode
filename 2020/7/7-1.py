9c1e386b64ab97193e610420a06ea9f0b7a4f58d
Eric Adler <Eric Adler>
2020-12-07 17:43:14 -0500

    2020: 7-1: Started over since I left my code at lunch... Not quite there yet but here's where I got to at lunch.

--- 2020/7/7-1.py
+++ 2020/7/7-1.py
@@ -3,7 +3,13 @@ import os
 
 print "importing"
 
-inputfile_source = os.path.dirname(__file__) + "/input.txt"
+inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
+
+colors = []
+bags = []
+biggerbags = []
+
+bagtype = "shiny gold"
 
 def findthing1(testrange):
     seekvalue = 0
@@ -20,16 +26,32 @@ def findthing2(testrange):
 def findgroup(testgroup):
     return (findthing1(testgroup[1]) + findthing2(testgroup[2]))
 
-def checkeverything(filename):
+def checkeverything(filename, checkfor):
     inputfile = open(filename)
-    inputfiledata = inputfile.readlines()
-    inputdata = inputfiledata.split("\n")
+    inputdata = inputfile.readlines()
+    
+    print("Parsing data")
     for line in inputdata:
-        result1 = findthing1(line)
-        result2 = findthing2(line)
-        result3 = findgroup(line)
-    print("Result %s" % result1)
-    print("Result %s" % result2)
-    print("Result %s" % result3)
+        line = line.replace("bags","bag").replace(" bag, ", "|").replace(" bag contain no other bag.","").replace(" bag contain ","|").replace(" bag.","").replace("\n","")
+        lineitems = line.split("|")
+        bag = []
+        for index, item in enumerate(lineitems):
+            if index == 0:
+                colors.append(item)
+                bag.append(item)
+            else:
+                item = item.split(" ",1)
+                bag.append([int(item[0]), item[1]])
+        bags.append(bag)
+    #print(bags)
 
-checkeverything(inputfile_source)
+    print("Checking bags")
+    for lineitems in bags:
+        #print(lineitems)
+        for index, item in enumerate(lineitems):
+            if index > 0:
+                if item[1] == checkfor:
+                    biggerbags.append(lineitems[0])
+    return biggerbags
+
+print(checkeverything(inputfile_source, bagtype))
