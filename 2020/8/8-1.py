4d301508251c76f416e3065cdcd592b54f3b5901
Eric Adler <Eric Adler>
2020-12-08 17:23:22 -0500

    2020: 8-1: runs against test input correctly

--- 2020/8/8-1.py
+++ 2020/8/8-1.py
@@ -6,10 +6,31 @@ print "importing"
 inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
 
 def codeexec(code):
+    maxline = len(code)
     acc = 0
-    for [command, argv] in code:
-        seekvalue = 1
-    return seekvalue
+    line = 0
+    hasrun = []
+
+    while line < maxline:
+        command = code[line][0]
+        argv = code[line][1]
+        if line in hasrun:
+            break
+        else:
+            hasrun.append(line)
+        print(command, argv)
+        if (command == "acc"):
+            acc += argv
+            line += 1
+        elif (command == "jmp"):
+            line += argv
+        elif (command == "nop"):
+            line += 1
+        else:
+            print("Error!  Command unknown.", command, argv)
+            break
+
+    return acc
 
 def checkeverything(filename):
     code = []
@@ -20,7 +41,7 @@ def checkeverything(filename):
         command = line[0:3]
         argv = int(line[4:])
         code.append([command,argv])
-    #return codeexec(code)
-    print(code)
+    return codeexec(code)
+    #print(code)
 
-checkeverything(inputfile_source)
+print(checkeverything(inputfile_source))
