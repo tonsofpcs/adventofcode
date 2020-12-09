34489b78f5eb7334e7f21808cbc5d6a1c0f5e4b1
Eric Adler <Eric Adler>
2020-12-09 00:12:27 -0500

    2020: 9-1

--- 2020/9/9-1.py
+++ 2020/9/9-1.py
@@ -3,15 +3,13 @@ import os
 
 print "importing"
 
-inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
-testlength = 5
+inputfile_source = os.path.dirname(__file__) + "/input.txt"
+testlength = 25
 
 def testpair(testrange, sumtocheck):
-    print(testrange, sumtocheck)
     success = 0
     for item in testrange:
         for item2 in testrange:
-            print(int(item.replace("\n","")), int(item2.replace("\n","")),(int(item.replace("\n","")) + int(item2.replace("\n",""))))
             if (int(item.replace("\n","")) + int(item2.replace("\n",""))) == int(sumtocheck):
                 success = 1
                 break
