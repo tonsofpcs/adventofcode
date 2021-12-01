ced13dd6f331440ae0bad299c6874bbbe3a76a05
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-01 18:07:31 -0500

    2021-1: test 2

--- 2021/1/structure-1.py
+++ 2021/1/structure-1.py
@@ -4,7 +4,7 @@ import copy
 
 print "importing"
 
-inputfile_source = os.path.dirname(__file__) + "/input.txt"
+inputfile_source = os.path.dirname(__file__) + "/testinput2.txt"
 
 def findthing1(testrange):
     seekvalue = 0
