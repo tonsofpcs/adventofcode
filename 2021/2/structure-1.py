9e465db2d76e07e0ca52129e603bc7b8e5f89011
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-03 08:15:32 -0500

    2021-2: first

--- 2021/2/structure-1.py
+++ 2021/2/structure-1.py
@@ -6,17 +6,22 @@ print("importing")
 
 inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
 
+directions = {
+    'forward': [1,0],
+    'backward': [-1,0],
+    'up': [0,-1],
+    'down': [0,1]
+}
+
 def findthing1(testrange):
-    seekvalue = 0
-    for item in testrange:
-        seekvalue = 1
+    seekvalue = []
+    seekvalue = testrange.split(" ")
     return seekvalue
 
 def findthing2(testrange):
-    seekvalue = 0
-    for item in testrange:
-        seekvalue = 1
-    return seekvalue
+    testval = findthing1(testrange)
+    hv = [directions[testval[0]][0] * int(testval[1]), directions[testval[0]][1] * int(testval[1])]
+    return hv
 
 def findgroup(testgroup):
     return (findthing1(testgroup[1]) + findthing2(testgroup[2]))
@@ -25,12 +30,22 @@ def checkeverything(filename):
     inputfile = open(filename)
     inputfiledata = inputfile.read()
     inputdata = inputfiledata.split("\n")
+
+    horiz = []
+    vert = []
+
     for line in inputdata:
-        result1 = findthing1(line)
-        result2 = findthing2(line)
-        result3 = findgroup(line)
-    print("Result %s" % result1)
-    print("Result %s" % result2)
-    print("Result %s" % result3)
+        hv = findthing2(line)
+        print(hv)
+        horiz.append(hv[0])
+        vert.append(hv[1])
+    
+    result1 = sum(horiz)
+    result2 = sum(vert)
+    result3 = result1 * result2
+
+    print("Horiz %s" % result1)
+    print("Depth %s" % result2)
+    print("Mult %s" % result3)
 
 checkeverything(inputfile_source)
