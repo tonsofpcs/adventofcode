10e86f5a7c3ce759e9682acaef1e1ce9f5eb084d
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-06 10:05:26 -0500

    2021-4: fix board selection, get diagonals

--- 2021/4/structure-1.py
+++ 2021/4/structure-1.py
@@ -34,15 +34,35 @@ def checkeverything(filename):
     print(callednumbers)
     for index in range(int((len(inputdata)-1)/6)):
         board = []
-        for line in inputdata[index*6+2:index*5+7]:
+        for line in inputdata[index*6+2:index*6+7]:
             board.append(re.split(" +",line.strip(" ")))
         boards.append(board)
 
+    diagonals1 = []
+    diagonals2 = []
+    for board in boards:
+        diagonals1.append(
+            [
+                board[0][0],
+                board[1][1],
+                board[2][2],
+                board[3][3],
+                board[4][4]
+            ])
+        diagonals2.append(
+            [
+                board[0][4],
+                board[1][3],
+                board[2][2],
+                board[3][1],
+                board[4][0]
+            ])
     #    result1 = findthing1(line)
     #    result2 = findthing2(line)
     #    result3 = findgroup(line)
 
     print(boards)
+    print(diagonals1, diagonals2)
 
     #print("Result %s" % result1)
     #print("Result %s" % result2)
