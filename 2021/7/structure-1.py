74438a8dfad1ef3f4b9e9eb43d1720430a15aa73
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-07 07:52:17 -0500

    2021-7: part 2 - cost calculation

--- 2021/7/structure-1.py
+++ 2021/7/structure-1.py
@@ -7,12 +7,17 @@ import statistics
 print("importing")
 
 inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
+costincrement = 3
+forcelocation = -1
 
-def findthing2(testrange):
-    seekvalue = 0
-    for item in testrange:
-        seekvalue = 1
-    return seekvalue
+def cost_distance(distance):
+    distance = abs(distance)
+    cost = 0
+    for index in range(distance+1):
+        cost += index
+
+    print("Distance %s = Cost %s" % (distance,cost))
+    return cost
 
 def checkeverything(filename):
     inputfile = open(filename)
@@ -31,16 +36,22 @@ def checkeverything(filename):
 
     print(statistics.median(crabs)) #crabsum*1.0/maxcrabs)
     #averagecrab = int(crabsum/maxcrabs)
-    averagecrab = int(statistics.median(crabs))
+
+    if forcelocation == -1:
+        averagecrab = int(statistics.median(crabs))
+    else:
+        averagecrab = forcelocation
+
     crabdistance = 0
+    cost = 0
 
     #for index, location in enumerate(abstractcrabs):
     for index in range(maxcrabs+1):
         crabdistance += abs(crabs.count(index)*(averagecrab - index))
+        cost += abs(crabs.count(index)*cost_distance(averagecrab - index))
     
-    
-    print("Result %s" % crabdistance)
-    #print("Result %s" % result2)
+    print("Distance %s" % crabdistance)
+    print("Cost %s" % cost)
     #print("Result %s" % result3)
 
 checkeverything(inputfile_source)
