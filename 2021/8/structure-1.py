56bfddf5122fbe880e2d34272c3964a882156824
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-08 09:19:01 -0500

    2021-8: I'm doing something wrong.

--- 2021/8/structure-1.py
+++ 2021/8/structure-1.py
@@ -32,6 +32,11 @@ def possibleremove(possibilities, whattokeep):
     # print("Keep: %s" % whattokeep)
     return re.sub("[" + whattokeep + "]", "", possibilities)
 
+def possiblecontain(possibilities, searchfor):
+    return bool(re.search("[" + searchfor + "]", possibilities))
+    # find = possibilities.find(searchfor)
+    # return (find != -1)
+
 def findnumbers(dataset):
     wirepossibilities = ["abcdefg"]*7
     for wireset in dataset:
@@ -47,7 +52,7 @@ def findnumbers(dataset):
                     wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
         elif numwires == 3:  #SEVEN, 1+3+6
             for wireid in range(7):
-                if wireid in [2, 5, 0]:
+                if wireid in [0, 2, 5]:
                     wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
                 else:
                     wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
@@ -61,6 +66,7 @@ def findnumbers(dataset):
             continue
         elif numwires == 6:  #zero, six, nine - tells nothing on its own
             continue
+    print(wirepossibilities)
     for wireset in dataset:
         #print(wirepossibilities)
         numwires = len(wireset)
@@ -69,10 +75,53 @@ def findnumbers(dataset):
         elif numwires <= 4:  #one, seven, four - already done
             continue
         elif numwires == 5:  #two, three, five
-            # if x in wirepossibilities[1]
-            continue
+            if possiblecontain(wirepossibilities[1],wireset) and possiblecontain(wirepossibilities[5],wireset) and (not(possiblecontain(wirepossibilities[2],wireset)) or not(possiblecontain(wirepossibilities[4],wireset))): #FIVE!
+                print("Five! %s" % wireset)
+                for wireid in range(7):
+                    if wireid in [0,1,3,5,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    elif wireid in [2,4]:
+                        wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            elif possiblecontain(wirepossibilities[2],wireset) and possiblecontain(wirepossibilities[4],wireset) and (not(possiblecontain(wirepossibilities[1],wireset)) or not(possiblecontain(wirepossibilities[5],wireset))): #TWO!
+                print("Two! %s" % wireid)
+                for wireid in range(7):
+                    if wireid in [0,2,3,4,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    elif wireid in [1,5]:
+                        wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            elif possiblecontain(wirepossibilities[2],wireset) and possiblecontain(wirepossibilities[5],wireset) and (not(possiblecontain(wirepossibilities[1],wireset)) or not(possiblecontain(wirepossibilities[4],wireset))): #TWO!
+                print("Three! %s" % wireid)
+                for wireid in range(7):
+                    if wireid in [0,2,3,5,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    elif wireid in [1,4]:
+                        wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            else:
+                print("Five wires but not 2,3,5?")
         elif numwires == 6:  #zero, six, nine
-            continue
+            if not possiblecontain(wirepossibilities[3],wireset): #ZERO
+                print("Zero! %s" % wireset)
+                for wireid in range(7):
+                    if wireid in [0,1,2,4,5,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    # elif wireid == 3:  #already there if we matched
+                    #     wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            elif not possiblecontain(wirepossibilities[2],wireset): #SIX
+                print("Six! %s" % wireset)
+                for wireid in range(7):
+                    if wireid in [0,1,3,4,5,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    # elif wireid == 2:  #already there if we matched
+                    #     wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            elif not possiblecontain(wirepossibilities[4],wireset): #NINE
+                print("Nine! %s" % wireset)
+                for wireid in range(7):
+                    if wireid in [0,1,2,3,5,6]:
+                        wirepossibilities[wireid] = possiblekeep(wirepossibilities[wireid],wireset)
+                    # elif wireid == 4:  #already there if we matched
+                    #     wirepossibilities[wireid] = possibleremove(wirepossibilities[wireid],wireset)
+            else:
+                print("Six wires but inconclusive. %s : %s" % (wireset, wirepossibilities))
     return wirepossibilities
 
 
@@ -93,8 +142,12 @@ def checkeverything(filename):
         dataset,raw = readline(line)
         numbermap = findnumbers(dataset)
 
+        lenset = []
+        for line in dataset:
+            lenset.append(len(line))
 
         print("Dataset: %s" % dataset)
+        print("Lengths: %s" % lenset)
         print("Raw Out: %s" % raw)
         print("Numbermap: %s" % numbermap)
 
