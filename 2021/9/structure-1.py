43887831795f6c250e5f6ace61ac6c66117fa5c0
tonsofpcs <34757554+tonsofpcs@users.noreply.github.com>
2021-12-09 18:35:21 -0500

    2021-9: part 1

--- 2021/9/structure-1.py
+++ 2021/9/structure-1.py
@@ -5,7 +5,7 @@ import sys
 
 print("importing")
 
-inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
+inputfile_source = os.path.dirname(__file__) + "/input.txt"
 
 def findthing1(testrange):
     seekvalue = 0
@@ -28,16 +28,32 @@ def checkeverything(filename):
     inputdata = inputfiledata.split("\n")
     array = []
     for line in inputdata:
-        array.append(list(line))
+        array.append(list(map(int,list(line))))
+    
+    print(array)
     
     width = len(array[0])
     height = len(array)
+    lowrisk = 0
     for y in range(height):
         for x in range(width):
-            print(array[y][x], end='')
-        print("")
+            adjacents = []
+            if x-1 > -1:
+                adjacents.append(array[y][x-1])
+            if y-1 > -1:
+                adjacents.append(array[y-1][x])
+            if x+1 < width:
+                adjacents.append(array[y][x+1])
+            if y+1 < height:
+                adjacents.append(array[y+1][x])
+            adjacents.sort()
+            thisone = array[y][x]
+            if thisone < adjacents[0]:
+                print(x, y, thisone, adjacents)
+                lowrisk += thisone + 1
 
-    # print("Result %s" % result1)
+
+    print("Result %s" % lowrisk)
     # print("Result %s" % result2)
     # print("Result %s" % result3)
 
