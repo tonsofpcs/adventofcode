80144ada5f41d620ad3e6d3e34cdb5d6032a7be8
tonsofpcs <tonsofpcs@tonsofpcs.com>
2024-12-03 17:13:10 -0500

    2024: 3-1

--- 2024/3/structure-1.py
+++ 2024/3/structure-1.py
@@ -6,31 +6,56 @@ print("importing")
 
 inputfile_source = os.path.dirname(__file__) + "/input.txt"
 
-def findthing1(testrange):
-    seekvalue = 0
-    for item in testrange:
-        seekvalue = 1
+def findvalues(testrange):
+    # check for validity, output values to multiply
+    seekvalue = []
+    testrange = testrange[1:]
+    testrange = testrange.split(",")
+    try:
+        teststr = str(int(testrange[0]))
+    except:
+        return [0,0]
+    if teststr == testrange[0]:
+        seekvalue.append(int(testrange[0]))
+    else:
+        return [0,0]
+    testrange = testrange[1].split(")")
+    try:
+        teststr = str(int(testrange[0]))
+    except:
+        return [0,0]
+    if teststr == testrange[0]:
+        seekvalue.append(int(testrange[0]))
+    else:
+        return [0,0]
+    # print(seekvalue)
     return seekvalue
 
-def findthing2(testrange):
-    seekvalue = 0
-    for item in testrange:
-        seekvalue = 1
-    return seekvalue
+    
 
-def findgroup(testgroup):
-    return (findthing1(testgroup[1]) + findthing2(testgroup[2]))
+def processvalues(testrange):
+    # multiply values
+    return testrange[0] * testrange[1]
 
 def checkeverything(filename):
     inputfile = open(filename)
     inputfiledata = inputfile.read()
-    inputdata = inputfiledata.split("\n")
+    if inputfiledata[0-3] == "mul":
+        offset = 0
+    else:
+        offset = 1
+    inputdata = inputfiledata.split("mul")
+    if offset == 1:
+        del inputdata[0]
+    multipliers = [0,0]
+    product = 0
+    summation = 0
     for line in inputdata:
-        result1 = findthing1(line)
-        result2 = findthing2(line)
-        result3 = findgroup(line)
-    print("Result %s" % result1)
-    print("Result %s" % result2)
-    print("Result %s" % result3)
+        multipliers = findvalues(line)
+        product = processvalues(multipliers)
+        summation += product
+    # print("Result %s" % multipliers)
+    print("Result %s" % product)
+    print("Result %s" % summation)
 
 checkeverything(inputfile_source)
