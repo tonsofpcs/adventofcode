0187f249280dbb49c79868457a053707c5e361d9
tonsofpcs <tonsofpcs@tonsofpcs.com>
2024-12-05 16:25:56 -0500

    2024-5: Prep, test split

---
+++ 2024/5/structure-1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import os
+import copy
+
+debugmode = True
+
+if debugmode: print("importing")
+
+inputfile_source = os.path.dirname(__file__) + "/testinput.txt"
+
+def findthing1(testrange):
+    seekvalue = 0
+    for item in testrange:
+        seekvalue = 1
+    return seekvalue
+
+def findthing2(testrange):
+    seekvalue = 0
+    for item in testrange:
+        seekvalue = 1
+    return seekvalue
+
+def findgroup(testgroup):
+    return (findthing1(testgroup[1]) + findthing2(testgroup[2]))
+
+def checkeverything(filename):
+    inputfile = open(filename)
+    inputfiledata = inputfile.read()
+    inputdata = inputfiledata.split("\n")
+    for linenum,line in enumerate(inputdata):
+        if(line == ""):
+            pagerules = inputdata[:linenum]
+            updates = inputdata[linenum+1:]
+            break
+    
+    if debugmode: print(pagerules)
+    if debugmode: print("########")
+    if debugmode: print(updates)
+
+    # for line in updates:
+    #     result1 = findthing1(line)
+    #     result2 = findthing2(line)
+    #     result3 = findgroup(line)
+    # print("Result %s" % result1)
+    # print("Result %s" % result2)
+    # print("Result %s" % result3)
+
+checkeverything(inputfile_source)
